a0d254a5e3278aa400ddcefbe706b364228016cb
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-01-18 16:43:13 +0100

    update

--- src/main/java/se/lexicon/java_springboot/dao/StudentDaoImpl.java
+++ src/main/java/se/lexicon/java_springboot/dao/StudentDaoImpl.java
@@ -1,46 +1,67 @@
 package se.lexicon.java_springboot.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import se.lexicon.java_springboot.model.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Collection;
 import java.util.Optional;
 
 @Repository
         public class StudentDaoImpl implements StudentDao{
-
+    @PersistenceContext
+       EntityManager entityManager;
+@Transactional
     @Override
     public Student persist(Student student) {
+        entityManager.persist(student);
         return null;
     }
-
+@Transactional(readOnly = true)
     @Override
     public Optional<Student> findById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(entityManager.find(Student.class,id));
     }
-
+@Transactional(readOnly = true)
     @Override
     public Optional<Student> findByEmail(String email) {
-        return Optional.empty();
+        return entityManager.createQuery("select s from Student s where UPPER(s.email) = UPPER(:e)", Student.class)
+                .setParameter("e", email)
+                .getResultStream()
+                .findFirst();
     }
-
+@Transactional(readOnly = true)
     @Override
     public Collection<Student> findByNameContains(String name) {
-        return null;
-    }
+        return entityManager.createQuery("select s from Student s " +
+                        "where " +
+                        "UPPER(s.firstName) LIKE UPPER(CONCAT('%', :name, '%'))" +
+                        " OR  " +
+                        "UPPER(s.lastName) LIKE UPPER(CONCAT('%', :name, '%'))", Student.class)
+                .setParameter("name", name)
+                .getResultList();
 
+    }
+@Transactional(readOnly = true)
     @Override
     public Collection<Student> findAll() {
-        return null;
+        return entityManager.
+                createQuery("select s from Student s", Student.class)
+                .getResultList();
     }
-
+@Transactional
     @Override
     public Student update(Student student) {
-        return null;
+    return entityManager.merge(student);
     }
-
+@Transactional
     @Override
     public void remove(String id) {
-
+        Student student = entityManager.find(Student.class, id);
+        if (student != null) entityManager.remove(student);
+        //else // throw exception...
     }
+
 }
