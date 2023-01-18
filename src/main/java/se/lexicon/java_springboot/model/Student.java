4e1cf43cb13ed1ab5a26b829c93c3869297681a5
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-01-18 15:06:34 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/java_springboot/model/Student.java
@@ -0,0 +1,115 @@
+package se.lexicon.java_springboot.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+//@Table(name = "student_table")
+public class Student {
+    /*@Id //Primary Key
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    //@Column(name = "STD_ID",updatable = false)
+    @Column(updatable = false)
+    private int studentId;*/
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    private  String id;
+    @Column(nullable = false,length= 100)
+    private String firstName;
+    @Column(nullable = false,length = 100)
+    private  String lastName;
+    @Column(nullable = false,length = 120,unique = true)
+    private  String email;
+    @Column(nullable = false)
+    private LocalDate birthDate;
+    private boolean status;
+    private LocalDateTime registrationDate;
+
+    public Student() {
+        this.status = true;
+        this.registrationDate = LocalDateTime.now();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return status == student.status && Objects.equals(id, student.id) && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(email, student.email) && Objects.equals(birthDate, student.birthDate) && Objects.equals(registrationDate, student.registrationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, birthDate, status, registrationDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", status=" + status +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
+}
